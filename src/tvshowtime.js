9e524d3b188da8cb5ef68a4df1edd89dba1ad850
Stephen Baker <ste.baker.92@gmail.com>
2017-11-22 00:27:55 +0000

    Implemented post show to tvshowtime

--- src/tvshowtime.js
+++ src/tvshowtime.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
 
-  var tvshowtime = function(user, pass, clientId) {
+  var tvshowtime = function (user, pass, clientId) {
     this.user = user;
     this.pass = pass;
     this.clientId = clientId;
@@ -9,7 +9,7 @@
     this.xhttp = new XMLHttpRequest();
   };
 
-  tvshowtime.prototype.makeCall = function(httpVerb, url, data, callback) {
+  tvshowtime.prototype.makeCall = function (httpVerb, url, data, callback) {
     var client = this;
     // some way to do this
     this.data = new FormData(data);
@@ -26,22 +26,47 @@
     this.xhttp.send(data ? serialize(data) : undefined);
   };
 
-  tvshowtime.prototype.register = function() {
+  tvshowtime.prototype.register = function () {
     data = {
       clientId: this.clientId,
       // redirectUri: '',
       state: ''
     };
 
-    this.makeCall("GET", "https://www.tvshowtime.com/oauth/authorize", data, function(res) {
+    this.makeCall("GET", "https://www.tvshowtime.com/oauth/authorize", data, function (res) {
       // get the code returned by the request and save it 
       data.code = res.code;
     });
 
-    this.makeCall("POST", this.baseUrl + '/v1/oauth/access_token', data, function(res){
+    this.makeCall("POST", this.baseUrl + '/v1/oauth/access_token', data, function (res) {
       this.accessToken = res.accessToken;
     });
   };
+
+  tvshowtime.prototype.postShow = function (netflixShow, success, error) {
+    var client = this;
+
+    this.makeCall("GET", this.baseUrl + "/v1/show?show_name=" + netflixShow.name, null, function (res) {
+      if (res == null) {
+        error("Unable to find show: " + netflixShow.name);
+        return;
+      }
+      var show = {
+        show_id: res.show.id,
+        season: netflixShow.season,
+        episode: netflixShow.ep
+      };
+      client.makeCall("POST", client.baseUrl + '/v1/show_progress', show, function (res) {
+        if (res == null) {
+          error("Unable to send " + netflixShow.name + "S" + netflixShow.season + "E" + netflixShow.ep + " to TvShowTime");
+        } else if (res.result === "OK") {
+          success("Sent " + netflixShow.name + "S" + netflixShow.season + "E" + netflixShow.ep + " to TvShowTime");
+        } else {
+          error("Unable to send " + netflixShow.name + "S" + netflixShow.season + "E" + netflixShow.ep + " to TvShowTime");
+        }
+      });
+    });
+  }
 
   function parseResponse(progressEvent) {
     return JSON.parse(progressEvent.currentTarget.response);
